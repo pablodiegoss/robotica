9c86226b126b4e55382b76578c998c907d703403
pablodiegoss <pablodiegoss@hotmail.com>
2018-04-27 11:35:55 -0300

    XABLAU

--- neymar.py
+++ neymar.py
@@ -4,11 +4,13 @@ m = ev3.Motor("outA")
 m2 = ev3.Motor("outB")
 m.run_direct(duty_cycle_sp=100)
 m2.run_direct(duty_cycle_sp=100)
-infrared = ev3.InfraredSensor('in3')
-infrared.MODE_IR_PROX
+infrared = ev3.InfraredSensor('in1')
 button = ev3.Button()
+infrared.auto_mode = False
+infrared.mode = infrared.MODE_IR_PROX
 
 while True:
+    print(infrared.proximity)
     if(infrared.proximity > 50):
         m.duty_cycle_sp = -100
     else:
