62f454e567363763e0a68fb6402302dbc41b11c6
pablodiegoss <pablodiegoss@hotmail.com>
2018-05-11 11:41:45 -0300

    Remove touch sensor

--- neymar.py
+++ neymar.py
@@ -1,5 +1,11 @@
 import ev3dev.ev3 as ev3
 import time
+
+
+def log(motor):
+    print("speed" + str(motor.speed))
+
+
 m = ev3.Motor("outA")
 m2 = ev3.Motor("outB")
 m3 = ev3.Motor("outD")
@@ -18,27 +24,29 @@ while True:
 
 time.sleep(5)
 
-m.run_direct(duty_cycle_sp=100)
-m2.run_direct(duty_cycle_sp=100)
-m3.run_direct(duty_cycle_sp=0)
+velocity = 100
 
+m.run_direct(duty_cycle_sp=velocity)
+m2.run_direct(duty_cycle_sp=velocity)
+m3.run_direct(duty_cycle_sp=0)
+activeMotor = m
 while True:
-    print(infrared.proximity)
+    print("Proximity :" + str(infrared.proximity))
     if(infrared.proximity > 50):
-        print(color.color)
-        if(color.color == color.COLOR_RED):
-            m2.duty_cycle_sp = 100
+        if(color.color == color.COLOR_BLACK):
+            activeMotor.duty_cycle_sp = 100
         else:
-            m2.duty_cycle_sp = -100
+            activeMotor.duty_cycle_sp = -100
         m3.duty_cycle_sp = 0
     else:
         if(infrared.proximity<25):
             m3.duty_cycle_sp = -100
-        m2.duty_cycle_sp = 100
-
+        activeMotor.duty_cycle_sp = 100
+        log(activeMotor)
     if button.any():
         break
 
 m.stop()
 m2.stop()
 m3.stop()
+
